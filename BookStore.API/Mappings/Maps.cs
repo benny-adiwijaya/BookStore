a3d40f37e79eb642be52c392ce474affe3ae4197
bennywijaya.18@gmail.com <59247546+benny-adiwijaya@users.noreply.github.com>
2020-07-24 19:13:04 +0700

    Created Data Classes and DTOs, automapper

---
+++ BookStore.API/Mappings/Maps.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AutoMapper;
+using BookStore.API.Data;
+using BookStore.API.DTOs;
+
+namespace BookStore.API.Mappings
+{
+    public class Maps : Profile
+    {
+
+        public Maps() { 
+            CreateMap<Author, AuthorDTO>().ReverseMap();
+            CreateMap<Book, BookDTO>().ReverseMap();
+        }
+    }
+}
